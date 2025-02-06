a100a4b18fefd7b59409603c92611219b02a0395
Jpvh03 <jpvh119thboss@gmail.com>
2025-02-06 12:56:26 +0200

    Completing welcome text effects

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,15 +32,30 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.bunifuGradientPanel1 = new Bunifu.Framework.UI.BunifuGradientPanel();
             this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.lblW = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.lblE = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.lblL = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.lblC = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.lblO = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.lblM = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.lblnameE = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuGradientPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // bunifuGradientPanel1
             // 
             this.bunifuGradientPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuGradientPanel1.BackgroundImage")));
             this.bunifuGradientPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuGradientPanel1.Controls.Add(this.lblnameE);
+            this.bunifuGradientPanel1.Controls.Add(this.lblM);
+            this.bunifuGradientPanel1.Controls.Add(this.lblO);
+            this.bunifuGradientPanel1.Controls.Add(this.lblC);
+            this.bunifuGradientPanel1.Controls.Add(this.lblL);
+            this.bunifuGradientPanel1.Controls.Add(this.lblE);
+            this.bunifuGradientPanel1.Controls.Add(this.lblW);
             this.bunifuGradientPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.Black;
-            this.bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.Black;
+            this.bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.DodgerBlue;
+            this.bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.SkyBlue;
             this.bunifuGradientPanel1.GradientTopLeft = System.Drawing.Color.Black;
             this.bunifuGradientPanel1.GradientTopRight = System.Drawing.Color.DarkGray;
             this.bunifuGradientPanel1.Location = new System.Drawing.Point(0, 0);
@@ -54,6 +69,92 @@
             this.bunifuElipse1.ElipseRadius = 30;
             this.bunifuElipse1.TargetControl = this;
             // 
+            // lblW
+            // 
+            this.lblW.AutoEllipsis = true;
+            this.lblW.AutoSize = true;
+            this.lblW.BackColor = System.Drawing.Color.Transparent;
+            this.lblW.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblW.ForeColor = System.Drawing.Color.White;
+            this.lblW.Location = new System.Drawing.Point(106, 27);
+            this.lblW.Name = "lblW";
+            this.lblW.Size = new System.Drawing.Size(49, 41);
+            this.lblW.TabIndex = 0;
+            this.lblW.Text = "W";
+            this.lblW.Enter += new System.EventHandler(this.bunifuCustomLabel1_Enter);
+            // 
+            // lblE
+            // 
+            this.lblE.AutoSize = true;
+            this.lblE.BackColor = System.Drawing.Color.Transparent;
+            this.lblE.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Bold);
+            this.lblE.ForeColor = System.Drawing.Color.White;
+            this.lblE.Location = new System.Drawing.Point(142, 27);
+            this.lblE.Name = "lblE";
+            this.lblE.Size = new System.Drawing.Size(36, 41);
+            this.lblE.TabIndex = 1;
+            this.lblE.Text = "E";
+            // 
+            // lblL
+            // 
+            this.lblL.AutoSize = true;
+            this.lblL.BackColor = System.Drawing.Color.Transparent;
+            this.lblL.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Bold);
+            this.lblL.ForeColor = System.Drawing.Color.White;
+            this.lblL.Location = new System.Drawing.Point(167, 27);
+            this.lblL.Name = "lblL";
+            this.lblL.Size = new System.Drawing.Size(33, 41);
+            this.lblL.TabIndex = 2;
+            this.lblL.Text = "L";
+            // 
+            // lblC
+            // 
+            this.lblC.AutoSize = true;
+            this.lblC.BackColor = System.Drawing.Color.Transparent;
+            this.lblC.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Bold);
+            this.lblC.ForeColor = System.Drawing.Color.White;
+            this.lblC.Location = new System.Drawing.Point(190, 27);
+            this.lblC.Name = "lblC";
+            this.lblC.Size = new System.Drawing.Size(45, 41);
+            this.lblC.TabIndex = 3;
+            this.lblC.Text = "C";
+            // 
+            // lblO
+            // 
+            this.lblO.AutoSize = true;
+            this.lblO.BackColor = System.Drawing.Color.Transparent;
+            this.lblO.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Bold);
+            this.lblO.ForeColor = System.Drawing.Color.White;
+            this.lblO.Location = new System.Drawing.Point(221, 27);
+            this.lblO.Name = "lblO";
+            this.lblO.Size = new System.Drawing.Size(47, 41);
+            this.lblO.TabIndex = 4;
+            this.lblO.Text = "O";
+            // 
+            // lblM
+            // 
+            this.lblM.AutoSize = true;
+            this.lblM.BackColor = System.Drawing.Color.Transparent;
+            this.lblM.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Bold);
+            this.lblM.ForeColor = System.Drawing.Color.White;
+            this.lblM.Location = new System.Drawing.Point(256, 27);
+            this.lblM.Name = "lblM";
+            this.lblM.Size = new System.Drawing.Size(49, 41);
+            this.lblM.TabIndex = 5;
+            this.lblM.Text = "M";
+            // 
+            // lblnameE
+            // 
+            this.lblnameE.AutoSize = true;
+            this.lblnameE.BackColor = System.Drawing.Color.Transparent;
+            this.lblnameE.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Bold);
+            this.lblnameE.ForeColor = System.Drawing.Color.White;
+            this.lblnameE.Location = new System.Drawing.Point(295, 27);
+            this.lblnameE.Name = "lblnameE";
+            this.lblnameE.Size = new System.Drawing.Size(36, 41);
+            this.lblnameE.TabIndex = 6;
+            this.lblnameE.Text = "E";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -63,6 +164,9 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.bunifuGradientPanel1.ResumeLayout(false);
+            this.bunifuGradientPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -71,6 +175,13 @@
 
         private Bunifu.Framework.UI.BunifuGradientPanel bunifuGradientPanel1;
         private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private Bunifu.Framework.UI.BunifuCustomLabel lblW;
+        private Bunifu.Framework.UI.BunifuCustomLabel lblE;
+        private Bunifu.Framework.UI.BunifuCustomLabel lblM;
+        private Bunifu.Framework.UI.BunifuCustomLabel lblO;
+        private Bunifu.Framework.UI.BunifuCustomLabel lblC;
+        private Bunifu.Framework.UI.BunifuCustomLabel lblL;
+        private Bunifu.Framework.UI.BunifuCustomLabel lblnameE;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,86 @@ namespace ToDoList_Port1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblW.Visible = false; //Set all label visibility to false for load of program
+            lblE.Visible = false;
+            lblL.Visible = false;
+            lblC.Visible = false;  
+            lblO.Visible = false;  
+            lblM.Visible = false;
+            lblnameE.Visible = false;
+            
+            //Using timer, to create fade in entrace effect for "WELCOME"
+            //W
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += (s, args) =>
+            {
+                lblW.Visible = true; 
+                timer.Stop(); 
+            };
+            timer.Start();
+            //E
+            Timer timer2 = new Timer();
+            timer2.Interval = 1200;
+            timer2.Tick += (s, args) =>
+            {
+                lblE.Visible = true;
+                timer2.Stop();
+            };
+            timer2.Start();
+            //L
+            Timer timer3 = new Timer();
+            timer3.Interval = 1400;
+            timer3.Tick += (s, args) =>
+            {
+                lblL.Visible = true;
+                timer3.Stop();
+            };
+            timer3.Start();
+            //C
+            Timer timer4 = new Timer();
+            timer4.Interval = 1600;
+            timer4.Tick += (s, args) =>
+            {
+                lblC.Visible = true;
+                timer4.Stop();
+            };
+            timer4.Start();
+            //O
+            Timer timer5 = new Timer();
+            timer5.Interval = 1800;
+            timer5.Tick += (s, args) =>
+            {
+                lblO.Visible = true;
+                timer5.Stop();
+            };
+            timer5.Start();
+            //M
+            Timer timer6 = new Timer();
+            timer6.Interval = 2000;
+            timer6.Tick += (s, args) =>
+            {
+                lblM.Visible = true;
+                timer6.Stop();
+            };
+            timer6.Start();
+            //E
+            Timer timer7 = new Timer();
+            timer7.Interval = 2200;
+            timer7.Tick += (s, args) =>
+            {
+                lblnameE.Visible = true;
+                timer7.Stop();
+            };
+            timer7.Start();
+        }
+
+        private void bunifuCustomLabel1_Enter(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
